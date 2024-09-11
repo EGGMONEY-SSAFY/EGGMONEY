0c4cb72d3b5cf36c10915bf225d72c25fcd6947c
황우성 <asdf1916@naver.com>
2024-09-11 11:44:36 +0900

    init: BaseTime 추가

--- backend/src/main/java/com/ssafy/eggmoney/EggmoneyApplication.java
+++ backend/src/main/java/com/ssafy/eggmoney/EggmoneyApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.eggmoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EggmoneyApplication {
 
 	public static void main(String[] args) {
