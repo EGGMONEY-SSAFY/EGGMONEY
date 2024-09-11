57b77de00830e8b96836064614c680ac54df1996
jiyoung <jiyoung990601@gmail.com>
2024-09-11 12:09:14 +0900

    init : 주식엔티티 설정

--- backend/src/main/java/com/ssafy/eggmoney/stock/entity/StockTradeLog.java
+++ backend/src/main/java/com/ssafy/eggmoney/stock/entity/StockTradeLog.java
@@ -1,19 +1,28 @@
 package com.ssafy.eggmoney.stock.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.ssafy.eggmoney.common.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@Getter
 @Table(name = "stock_trade_logs")
 @NoArgsConstructor(access = PROTECTED)
-public class StockTradeLog {
+public class StockTradeLog extends BaseTime {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "trade_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_holding_id")
+    private StockHodings stockHoldings;
+    private int tradeStatus;
+    private String tradeType;
+    private int tradeAmount;
+    private int tradePrice;
 }
