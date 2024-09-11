f73b76fb55f361e6e2a17326ee4dcb5e6c692b9e
jiyoung <jiyoung990601@gmail.com>
2024-09-11 14:23:10 +0900

    init : 엔티티설정

--- backend/src/main/java/com/ssafy/eggmoney/loan/entity/LoanLog.java
+++ backend/src/main/java/com/ssafy/eggmoney/loan/entity/LoanLog.java
@@ -1,19 +1,28 @@
 package com.ssafy.eggmoney.loan.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.ssafy.eggmoney.common.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+@Getter
 @Entity
 @Table(name = "loan_logs")
 @NoArgsConstructor(access = PROTECTED)
-public class LoanLog {
+public class LoanLog extends BaseTime {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "loan_log_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "loan_id")
+    private Loan loan;
+
+    private int paymentMoney;
+
+
 }
--- backend/src/main/java/com/ssafy/eggmoney/withdrawal/entity/Withdrawal.java
+++ backend/src/main/java/com/ssafy/eggmoney/withdrawal/entity/Withdrawal.java
@@ -1,9 +1,8 @@
 package com.ssafy.eggmoney.withdrawal.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.ssafy.eggmoney.common.entity.BaseTime;
+import com.ssafy.eggmoney.user.entity.User;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -12,8 +11,18 @@ import static lombok.AccessLevel.PROTECTED;
 @Entity
 @Table(name = "withdrawals")
 @NoArgsConstructor(access = PROTECTED)
-public class Withdrawal {
+public class Withdrawal extends BaseTime {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "withdrawal_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private int withdrawalPrice;
+    private String withdrawalStatus;
+
+
 }
