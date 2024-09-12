7c154759b54813659cd88f95c2eb316a255ff577
jiyoung <jiyoung990601@gmail.com>
2024-09-12 14:12:03 +0900

    feat: 예금생성

---
+++ backend/src/main/java/com/ssafy/eggmoney/deposit/service/DepositService.java
@@ -0,0 +1,9 @@
+package com.ssafy.eggmoney.deposit.service;
+
+import com.ssafy.eggmoney.deposit.dto.requestdto.DepositCreateRequestDto;
+import com.ssafy.eggmoney.deposit.dto.responsedto.DepositResponseDto;
+
+public interface DepositService {
+    void createDeposit(DepositCreateRequestDto requestDto);
+//    DepositResponseDto getDeposit(long id);
+}
