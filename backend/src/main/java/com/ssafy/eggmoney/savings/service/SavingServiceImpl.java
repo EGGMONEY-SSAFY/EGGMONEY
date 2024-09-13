e1580f5bdce81727325fc4d5fb144f1744751b58
jiyoung <jiyoung990601@gmail.com>
2024-09-13 15:56:32 +0900

    feat: 적금가입하기

--- backend/src/main/java/com/ssafy/eggmoney/savings/service/SavingServiceImpl.java
+++ backend/src/main/java/com/ssafy/eggmoney/savings/service/SavingServiceImpl.java
@@ -1,12 +1,19 @@
 package com.ssafy.eggmoney.savings.service;
 
+import com.ssafy.eggmoney.account.entity.AccountLogType;
+import com.ssafy.eggmoney.account.service.AccountLogService;
+import com.ssafy.eggmoney.savings.dto.requestDto.SavingsCreateRequestDto;
 import com.ssafy.eggmoney.savings.dto.responseDto.SavingsProductListResponseDto;
+import com.ssafy.eggmoney.savings.entity.Savings;
 import com.ssafy.eggmoney.savings.entity.SavingsProduct;
 import com.ssafy.eggmoney.savings.repository.SavingsProductRepository;
 import com.ssafy.eggmoney.savings.repository.SavingsRepository;
+import com.ssafy.eggmoney.user.entity.User;
+import com.ssafy.eggmoney.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,7 +23,8 @@ public class SavingServiceImpl implements SavingService {
 
     private final SavingsRepository savingsRepository;
     private final SavingsProductRepository savingsProductRepository;
-
+    private final UserRepository userRepository;
+    private final AccountLogService accountLogService;
 
     @Override
     public List<SavingsProductListResponseDto> getSavingProducts() {
@@ -32,6 +40,27 @@ public class SavingServiceImpl implements SavingService {
                 ).collect(Collectors.toList());
 
         return productListDto;
+    }
+
+    @Override
+    public void createSaving(SavingsCreateRequestDto requestDto){
+
+        User user = userRepository.findById(requestDto.getUserId()).orElse(null);
+        SavingsProduct savingsProduct = savingsProductRepository.findById(requestDto.getSavingsProductId()).orElse(null);
+
+
+        Savings savings = Savings.builder()
+                .user(user)
+                .savingsProduct(savingsProduct)
+                .expireDate(LocalDateTime.now().plusMonths(savingsProduct.getSavingsDate()))
+                .paymentDate(LocalDateTime.now().getDayOfMonth())
+                .balance(requestDto.getPrice())
+                .build();
+
+        savingsRepository.save(savings);
+
+        accountLogService.createAccountLog(user.getId(),  AccountLogType.SAVINGS, -1 * requestDto.getPrice());
+
     }
 
 }
