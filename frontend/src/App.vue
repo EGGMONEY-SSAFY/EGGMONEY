8e5906979d83b68506f3b455ff48b2e201117796
Soyoung <bayleaf07@naver.com>
2024-09-14 22:55:33 +0900

    feat : 젠킨스 npm 버전 업데이트 후 다시 테스트

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,9 +11,9 @@ import { computed } from "vue"
     <div class="main-container bg-gray-200">
       <NavBarTop />
       <RouterView class="mt-14 mb-16" />
-      <p>변경이 있습니다요dhdhdxfgsfdgdddddddddddddsdfgh</p>
-      <p>코드를 ㅂㅁ냐어ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ릳</p>
-      <p>djjdjdj</p>
+      <p>변경이 있습니다요asdfasdfdhdhdxfgsfdgdddddddddddddsdfgh</p>
+      <p>코드asdf를 ㅂㅁ냐어aㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ릳</p>
+      <p>djjdjasdfdj</p>
       <NavBar />
     </div>
   </div>
