e6a95b09e6f3e20b28c80f49390ebbb2611b4ee0
정소영 <bayleaf07@naver.com>
2024-09-19 11:22:36 +0900

    Revert "Merge branch 'revert-613f9a9a' into 'develop'"
    This reverts merge request !33

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,9 @@ import { computed } from "vue"
     <div class="main-container bg-gray-200">
       <NavBarTop />
       <RouterView class="mt-14 mb-16" />
-      <p>하이</p>
+      <p>변경이 있습니다요asdfasdfdhdhdxfgsfdgdddddddddddddsdfgh</p>
+      <p>코드asdf를 ㅂㅁ냐어aㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ릳</p>
+      <p>djjdjasdfdj</p>
       <NavBar />
     </div>
   </div>
