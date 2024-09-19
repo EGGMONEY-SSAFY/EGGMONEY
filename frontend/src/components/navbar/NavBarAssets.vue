2395a7c6d1da01db3ed77b1f58aa5c409934442f
곽재은 <gje960217@naver.com>
2024-09-19 14:42:52 +0900

    Revert "Merge branch 'front/stock' into 'develop'"
    This reverts merge request !37

--- frontend/src/components/navbar/NavBarAssets.vue
+++ frontend/src/components/navbar/NavBarAssets.vue
@@ -6,12 +6,12 @@ import IconAssetsSelected from "../icons/IconAssetsSelected.vue"
 
 const route = useRoute()
 const isActive = computed(() => {
-  return route.path.includes("/assets")
+  return route.path.includes("/asset")
 })
 </script>
 
 <template>
-  <RouterLink to="/assets" class="flex flex-col justify-center items-center mt-2">
+  <RouterLink to="/asset" class="flex flex-col justify-center items-center mt-2">
     <component :is="isActive ? IconAssetsSelected : IconAssets" />
     <p class="text-sm mt-1">자산</p>
   </RouterLink>
