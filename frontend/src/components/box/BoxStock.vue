83976765b52c70911177e188d540b11293a37b59
unknown <gje960217@naver.com>
2024-09-19 18:00:47 +0900

    임시

---
+++ frontend/src/components/box/BoxStock.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import IconRightArrow from "../icons/IconRightArrow.vue"
+
+const props = defineProps({
+  stock: String,
+  price: { type: Number, default: 1 },
+  prePrice: { type: Number, default: 1 },
+})
+const gap = props.price - props.prePrice
+const rate = gap / props.price * 100
+</script>
+
+<template>
+  <div class="bg-white m-4 rounded-lg shadow flex justify-between">
+    <div class="m-6 flex self-center text-xl font-bold">{{ props.stock }}</div>
+		<div class="flex flex-col justify-center items-center ml-24">
+			<div>{{ props.price }}알</div>
+			<div class="text-xs" :class="gap > 0 ? 'text-red-500' : 'text-blue-500'">{{gap > 0 ? '+' + gap : gap}} ({{ Math.round(rate * 100) / 100}}%)</div>
+		</div>
+    <div class="m-6 flex justify-center items-center gap-4">
+      <IconRightArrow class="size-6" />
+    </div>
+  </div>
+</template>
