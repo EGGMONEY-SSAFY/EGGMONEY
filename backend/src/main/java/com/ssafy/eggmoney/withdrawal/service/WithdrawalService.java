851d5dd7f98404da1b3ea098c74ebdc909a51bf2
황우성 <asdf1916@naver.com>
2024-09-20 15:41:09 +0900

    feat: 금융망 API 계좌이체 기능 및 예외처리 + APIClient Class 추가

--- backend/src/main/java/com/ssafy/eggmoney/withdrawal/service/WithdrawalService.java
+++ backend/src/main/java/com/ssafy/eggmoney/withdrawal/service/WithdrawalService.java
@@ -1,12 +1,15 @@
 package com.ssafy.eggmoney.withdrawal.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.eggmoney.account.entity.Account;
 import com.ssafy.eggmoney.account.entity.AccountLogType;
 import com.ssafy.eggmoney.account.repository.AccountRepository;
 import com.ssafy.eggmoney.account.service.AccountService;
+import com.ssafy.eggmoney.common.webClient.ApiClient;
 import com.ssafy.eggmoney.family.dto.response.GetFamilyResponseDto;
 import com.ssafy.eggmoney.family.entity.Family;
-import com.ssafy.eggmoney.family.repository.FamilyRepository;
+import org.springframework.web.reactive.function.client.WebClient;
 import com.ssafy.eggmoney.user.dto.response.GetUserResponseDto;
 import com.ssafy.eggmoney.user.entity.User;
 import com.ssafy.eggmoney.user.repository.UserRepository;
@@ -16,12 +19,14 @@ import com.ssafy.eggmoney.withdrawal.dto.request.JudgeWithdrawalRequestDto;
 import com.ssafy.eggmoney.withdrawal.dto.response.GetWithdrawalResponseDto;
 import com.ssafy.eggmoney.withdrawal.entity.Withdrawal;
 import com.ssafy.eggmoney.withdrawal.entity.WithdrawalStatus;
-import com.ssafy.eggmoney.withdrawal.entity.WithdrawalType;
 import com.ssafy.eggmoney.withdrawal.repository.WithdrawalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 
+import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,6 +38,11 @@ public class WithdrawalService {
     private final UserRepository userRepository;
     private final AccountService accountService;
     private final AccountRepository accountRepository;
+    private final WebClient.Builder webClientBuilder;
+    private final ObjectMapper objectMapper;
+    private final ApiClient apiClient;
+
+    private BigInteger institutionTransactionUniqueNo = new BigInteger("0");
 
 //  출금요청 로그 조회
     public List<GetWithdrawalResponseDto> getWithdrawalLogs(GetWithdrawalRequestDto dto){
@@ -103,7 +113,7 @@ public class WithdrawalService {
         withdrawalRepository.save(
                 Withdrawal.builder()
                     .user(user)
-                    .withdrawalStatus(WithdrawalStatus.APPROVAL)
+                    .withdrawalStatus(WithdrawalStatus.PROGRESS)
                     .withdrawalPrice(dto.getPrice())
                 .build()
         );
@@ -113,6 +123,7 @@ public class WithdrawalService {
 //    출금 심사
     public void judgeWithdrawal(Long Wid, JudgeWithdrawalRequestDto dto){
         Withdrawal with = withdrawalRepository.findById(Wid).get();
+        User user = userRepository.findById(dto.getUserId()).get();
         String judge = dto.getJudge();
 
 //        예외처리: 부모가 아닌데 심사하려는 경우
@@ -129,10 +140,48 @@ public class WithdrawalService {
             return;
         }
 
-//      승인
-        if ( judge.equals("승인") ) {
-            with.setWithdrawalStatus(WithdrawalStatus.APPROVAL);
-            accountService.updateAccount(AccountLogType.WITHDRAWL, with.getUser().getId(), -with.getWithdrawalPrice());
+//      대출 승인
+        if (judge.equals("승인")) {
+            // UserKey 조회
+            apiClient.findUserKey(user.getEmail())
+                    .flatMap( userKeyResponse -> {
+                        HashMap<String, Object> responseMap = apiClient.parseResponse(userKeyResponse, new TypeReference<HashMap<String, Object>>() {});
+                        String userKey = String.valueOf(responseMap.get("userKey"));
+                        System.out.println(userKey);
+                        institutionTransactionUniqueNo = institutionTransactionUniqueNo.add(BigInteger.ONE);
+
+                        // 계좌 잔고 조회
+                        String transactionUniqueNo = apiClient.generateTransactionUniqueNo(institutionTransactionUniqueNo);
+
+                        return apiClient.getAccountBalance(userKey, transactionUniqueNo)
+                                .flatMap( accountBalanceResponse ->
+                                {
+                                    HashMap<String, Object> balanceResponseMap = apiClient.parseResponse(accountBalanceResponse, new TypeReference<HashMap<String, Object>>() {});
+                                    List<HashMap<String, Object>> recList = (List<HashMap<String, Object>>) balanceResponseMap.get("REC");
+                                    HashMap<String, Object> innerMap = recList.get(0);
+
+                                    // 잔고 확인 후 처리
+                                    if (Integer.parseInt(String.valueOf(innerMap.get("accountBalance"))) < with.getWithdrawalPrice()) {
+                                        System.out.println("실물계좌 잔고 부족");
+                                        return Mono.empty();
+                                    }
+
+                                    // 잔고가 충분하면 계좌 이체 수행
+                                    String childAccount = with.getUser().getRealAccount();
+                                    System.out.println("CHILD : "+childAccount);
+                                    String parentAccount = user.getRealAccount();
+                                    return apiClient.transferAccount(userKey, childAccount, parentAccount, transactionUniqueNo, with.getWithdrawalPrice());
+                                });
+                    })
+                    .subscribe( transferResponse -> {
+                        System.out.println("계좌이체 성공");
+                        // 대출 인스턴스 상태 변경 및 계좌 로그 갱신
+                        with.setWithdrawalStatus(WithdrawalStatus.APPROVAL);
+                        accountService.updateAccount(AccountLogType.WITHDRAWL, with.getUser().getId(), -with.getWithdrawalPrice());
+                        withdrawalRepository.save(with);
+                    }, error -> {
+                        System.out.println("Error: " + error.getMessage());
+                    });
         }
 
 //      거절
