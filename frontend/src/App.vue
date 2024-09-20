74f85cd2a0bc3e428231de96ae1054a56dab5e21
Soyoung <bayleaf07@naver.com>
2024-09-20 10:42:02 +0900

    feat: 웹훅 오타 수정 후 배포테스트3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,7 +14,7 @@ import { computed } from "vue"
       <NavBar />
     </div>
   </div>
-  <!-- asasdfasdfasdfasdfdsafdasdfasdfasdfdf sadf-->
+  <!-- asasdfasdfasdfasdfdsafdasdfasdfasasaaaasdfdf sadf-->
 </template>
 
 <style sc>
