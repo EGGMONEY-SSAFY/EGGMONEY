1e5f41f4694afd73030e47dcb5bb237dfc7a14d8
Soyoung <bayleaf07@naver.com>
2024-09-21 15:58:49 +0900

    feat: 배포 테스트7

--- backend/src/main/java/com/ssafy/eggmoney/account/controller/AccountController.java
+++ backend/src/main/java/com/ssafy/eggmoney/account/controller/AccountController.java
@@ -16,7 +16,7 @@ public class AccountController {
     private final AccountService accountService;
     private final AccountLogService accountLogService;
 
-//    메인계좌 조회fdddddddddddddddddddddddddd
+//    메인계좌 조회fdddddddddddfffddddddddddddddd
     @GetMapping("/{userId}")
     public GetAccountResponseDto getAccount(@PathVariable("userId") Long userId){
 
