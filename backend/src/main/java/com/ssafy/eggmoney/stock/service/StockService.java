91699a1b7ece62a54cc56b9060ba97f4d7f842c9
ShinBeomKim <shinbeomkim11@gmail.com>
2024-09-21 19:17:34 +0900

    feat: 국내업종 일자별지수 가져와서 db에 저장하기 완료

--- backend/src/main/java/com/ssafy/eggmoney/stock/service/StockService.java
+++ backend/src/main/java/com/ssafy/eggmoney/stock/service/StockService.java
@@ -1,7 +1,12 @@
 package com.ssafy.eggmoney.stock.service;
 
+import com.ssafy.eggmoney.stock.dto.response.StockPriceResponse;
 import com.ssafy.eggmoney.stock.dto.response.StockTokenResponse;
+
+import java.util.List;
 
 public interface StockService {
     StockTokenResponse getAccessToken();
+    List<StockPriceResponse> getStockPrices(String token, String inputDate, String stockCode);
+    void saveStockPrices(String token, String inputDate, String stockCode);
 }
