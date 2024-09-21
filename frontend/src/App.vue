527e441f565e1b03d3e1b64b40250d4539b75535
Soyoung <bayleaf07@naver.com>
2024-09-21 21:50:15 +0900

    feat: application.properties 설정2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,7 @@ import { computed } from "vue"
     <div class="main-container bg-gray-200">
       <NavBarTop />
       <RouterView class="mt-14 mb-16" />
-      <p>하이dddddjjjdddddd</p>
+      <p>하이dddddjjjㅇㅇdddddd</p>
       <NavBar />
     </div>
   </div>
