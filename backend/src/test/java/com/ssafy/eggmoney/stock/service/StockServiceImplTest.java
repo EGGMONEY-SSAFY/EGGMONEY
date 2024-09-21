c7976c63398a8d9edb7e61bd0a2b74ec18f93cde
ShinBeomKim <shinbeomkim11@gmail.com>
2024-09-22 03:18:46 +0900

    feat: 스케쥴링으로 평일마다 주식 종목별로 가격을 외부 api로 받아와서 db에 저장하는 기능 추가

--- backend/src/test/java/com/ssafy/eggmoney/stock/service/StockServiceImplTest.java
+++ backend/src/test/java/com/ssafy/eggmoney/stock/service/StockServiceImplTest.java
@@ -2,6 +2,7 @@ package com.ssafy.eggmoney.stock.service;
 
 import com.ssafy.eggmoney.stock.dto.response.StockPriceResponse;
 import com.ssafy.eggmoney.stock.dto.response.StockTokenResponse;
+import com.ssafy.eggmoney.stock.entity.StockItem;
 import com.ssafy.eggmoney.stock.repository.StockRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,9 +23,15 @@ import java.util.List;
 
 @SpringBootTest
 class StockServiceImplTest {
-//    @Autowired StockService stockService;
-//    @Autowired StockRepository stockRepository;
-//
+    @Autowired StockService stockService;
+    @Autowired StockRepository stockRepository;
+    String[] stockCodes = {"0001", "1001", "4002", "4003", "4004", "4005", "4007",
+            "4008", "4011", "4016", "4063", "4064", "4065"};
+    StockItem[] stockItems = {StockItem.KOSPI, StockItem.KOSDAQ, StockItem.AUTOMOTIVE,
+            StockItem.SEMICONDUCTOR, StockItem.HEALTHCARE, StockItem.BANKING, StockItem.ENERGY_CHEMICAL,
+            StockItem.STEEL, StockItem.CONSTRUCTION, StockItem.TRANSPORTATION, StockItem.MEDIA_ENTERTAINMENT,
+            StockItem.IT, StockItem.UTILITIES};
+
 //    @Test
 //    void getToken() {
 //        StockTokenResponse stockToken = stockService.getAccessToken();
@@ -81,8 +89,32 @@ class StockServiceImplTest {
 //            throw new RuntimeException(e);
 //        }
 //
-//        stockService.saveStockPrices(token, "20231126", "1001");
+//        for (int i = 11; i < stockCodes.length; i++) {
+//            List<StockPriceResponse> stockPrices = stockService.getStockPrices(token, "20240921", stockCodes[i]);
+//            stockService.saveStockPrices(stockPrices, stockItems[i]);
+//        }
+//    }
 //
-//        Assertions.assertThat(stockRepository.findById(100L)).isNotNull();
+//    @Test
+//    void getCurrentStockPrice() {
+//        String token;
+//        Path path = Paths.get("C:/Temp/stock_token.txt");
+//        try {
+//            BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+//            Instant lastModifiedTime = basicFileAttributes.lastModifiedTime().toInstant();
+//            Instant now = Instant.now();
+//
+//            if(lastModifiedTime.isBefore(now.minus(24, ChronoUnit.HOURS))) {
+//                getToken();
+//            }
+//
+//            token = String.join("\n", Files.readAllLines(path));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        BigDecimal currentStockPrice = stockService.getCurrentStockPrice(token, "0001");
+//
+//        Assertions.assertThat(currentStockPrice).isNotNull();
 //    }
 }
