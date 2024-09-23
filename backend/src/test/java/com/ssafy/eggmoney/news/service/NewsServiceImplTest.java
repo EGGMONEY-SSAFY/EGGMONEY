eaa1777d12292cf6daf4aa6a1861600a0aba8ee9
ShinBeomKim <shinbeomkim11@gmail.com>
2024-09-24 02:53:29 +0900

    feat: 기사내용 openAI api로 요청해서 응답 받는 기능 추가

--- backend/src/test/java/com/ssafy/eggmoney/news/service/NewsServiceImplTest.java
+++ backend/src/test/java/com/ssafy/eggmoney/news/service/NewsServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.ssafy.eggmoney.news.service;
 
 import com.ssafy.eggmoney.news.dto.response.NewsCrawlResponse;
+import com.ssafy.eggmoney.news.dto.response.SummarizedContentResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,33 @@ class NewsServiceImplTest {
 //        List<NewsCrawlResponse> headLineNews = newsService.crawlFinanceHeadLineNews();
 //        Assertions.assertThat(headLineNews).hasSizeGreaterThan(0);
 //    }
-//
+
 //    @Test
 //    void crawlFinanceNews() {
 //        List<NewsCrawlResponse> headLineNews = newsService.crawlFinanceHeadLineNews();
 //        for (NewsCrawlResponse news : headLineNews) {
-//            String s = newsService.crawlNewsContent(news.getUrl());
+//            String newsContent = newsService.crawlNewsContent(news.getUrl());
+//            System.out.println(news.getUrl());
+//            System.out.println(newsContent);
+//            SummarizedContentResponse summarizedContent = newsService.summarizeNews(newsContent);
+//            System.out.println(summarizedContent.getContent());
+//            System.out.println(summarizedContent.getRefusal());
+//            System.out.println(summarizedContent.getFinishReason());
+//            System.out.println(summarizedContent.getPromptTokens());
+//            System.out.println(summarizedContent.getCompletionTokens());
+//        }
+//
+//        for (int i = 0; i < 2; i++) {
+//            String newsContent = newsService.crawlNewsContent(headLineNews.get(i).getUrl());
+//            System.out.println(headLineNews.get(i).getUrl());
+//            System.out.println(newsContent);
+//            newsContent = "줄바꿈 X, 350토큰이상, 존댓말. 뉴스: " + newsContent;
+//            SummarizedContentResponse summarizedContent = newsService.summarizeNews(newsContent);
+//            System.out.println(summarizedContent.getContent());
+//            System.out.println(summarizedContent.getRefusal());
+//            System.out.println(summarizedContent.getFinishReason());
+//            System.out.println(summarizedContent.getPromptTokens());
+//            System.out.println(summarizedContent.getCompletionTokens());
 //        }
 //    }
 }
