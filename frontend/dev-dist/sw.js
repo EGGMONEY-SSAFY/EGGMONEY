f6a01b1523d13b2b0a709c141ae359ef04d8f34e
나경준 <na6037@naver.com>
2024-09-23 15:27:20 +0900

    fix: 오류 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l3hscetuofg"
+    "revision": "0.rp06fi0bpj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
