de99daee17595674e3171c188b572dea427d915d
jiyoung <jiyoung990601@gmail.com>
2024-09-23 11:42:26 +0900

    temp: NavBar 머지작업

--- frontend/src/views/Fin/FinView.vue
+++ frontend/src/views/Fin/FinView.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import BoxProduct from "@/components/box/BoxProduct.vue"
+import NavBarTab from "@/components/navbar/NavBarTab.vue"
+</script>
+
 <template>
   <div class="">
     <h1>This is an fin page</h1>
+    <NavBarTab />
+    <BoxProduct :deposit-date="0" , :deposit-rate="3.2" />
   </div>
 </template>
