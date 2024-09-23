43fc660dbe606baba67aa06f724475a0fef11698
Soyoung <bayleaf07@naver.com>
2024-09-23 11:53:30 +0900

    feat: 빌드 테스트2

--- backend/src/test/java/com/ssafy/eggmoney/stock/service/StockServiceImplTest.java
+++ backend/src/test/java/com/ssafy/eggmoney/stock/service/StockServiceImplTest.java
@@ -32,7 +32,7 @@ class StockServiceImplTest {
             StockItem.STEEL, StockItem.CONSTRUCTION, StockItem.TRANSPORTATION, StockItem.MEDIA_ENTERTAINMENT,
             StockItem.IT, StockItem.UTILITIES};
 
-//    @Test
+//    @Test33
 //    void getToken() {
 //        StockTokenResponse stockToken = stockService.getAccessToken();
 //
