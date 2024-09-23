ff976a8579547b8c9f86960848cbd41589f114d3
Soyoung <bayleaf07@naver.com>
2024-09-23 17:27:49 +0900

    feat: 배포테스트

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ import NavBarTop from "./components/navbar/NavBarTop.vue"
   <div class="flex justify-center bg-gray-800">
     <div class="main-container bg-gray-200">
       <NavBarTop />
-      <!-- ddddddsss -->
+      <!-- ddddddddddddsss -->
       <RouterView class="mt-12 mb-24" />
       <NavBar />
     </div>
