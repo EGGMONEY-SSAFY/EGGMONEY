db75a2a4434d20fe74b66b32474cda057200a752
황우성 <asdf1916@naver.com>
2024-09-24 13:44:34 +0900

    fix:build error

--- frontend/src/views/Asset/AssetMainView.vue
+++ frontend/src/views/Asset/AssetMainView.vue
@@ -3,13 +3,11 @@ import { onMounted } from 'vue';
 
     const props = defineProps({'child' : Object})
     onMounted ( () => {
-            console.log(child.email)
         }
     )
         
 </script>
 <template>
     <div>
-        {{ child.email }}
     </div>
 </template>
