452b9ab26224482dd14df774c7a8d4ef8c390370
나경준 <na6037@naver.com>
2024-09-24 17:46:35 +0900

    fix: 간편비밀번호 패드 너무 안이뻐 이쁘게 바꾸는 중^^

--- backend/src/main/java/com/ssafy/eggmoney/simplepwd/controller/PinPadController.java
+++ backend/src/main/java/com/ssafy/eggmoney/simplepwd/controller/PinPadController.java
@@ -1,12 +1,15 @@
 package com.ssafy.eggmoney.simplepwd.controller;
 
+import com.ssafy.eggmoney.global.dto.ResponseApi;
 import com.ssafy.eggmoney.simplepwd.dto.response.PinPadResponse;
 import com.ssafy.eggmoney.simplepwd.service.EncryptionService;
 import com.ssafy.eggmoney.simplepwd.service.PinPadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -23,5 +26,23 @@ public class PinPadController {
     @GetMapping(value = "/api/pinpad", produces = "application/json")
     public PinPadResponse getPinPad() throws Exception{
         return pinPadService.generatePinPadResponse();
+    }
+
+    @GetMapping("/api/public-key")
+    public String getPublicKey(){
+        return Base64.getEncoder().encodeToString(encryptionService.getPublicKey().getEncoded());
+    }
+    @PostMapping("/api/pinpad/verify")
+    public String verifyPin(@RequestBody String encryptyedPassword) throws Exception{
+        byte[] decodedPassword = Base64.getDecoder().decode(encryptyedPassword);
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, encryptionService.getPrivateKey());
+        byte[] decryptedPassword = chiper.doFinal(decodedPassword);
+
+        String decryptedPasswordString = new String(decryptedPassword, StandardCharsets.UTF_8);
+        System.out.println("복호화된 비밀번호:" + decryptedPasswordString);
+
+        return ResponseApi.success("비밀번호 검증 성공");
     }
 }
