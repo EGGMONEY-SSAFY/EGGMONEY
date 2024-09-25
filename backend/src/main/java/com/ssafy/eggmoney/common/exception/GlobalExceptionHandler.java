fb89b142e08184dcef4e9424f7b4d5e862f52ee8
KimShinBeom <gotwo6141922@naver.com>
2024-09-25 22:50:21 +0900

    refactor: GlobalExceptionHandler 수정

---
+++ backend/src/main/java/com/ssafy/eggmoney/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.ssafy.eggmoney.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleExceptions(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
