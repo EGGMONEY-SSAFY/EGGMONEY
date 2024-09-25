acc0d5fc196706a908fa8d4632384c72dd6bdf46
KimShinBeom <gotwo6141922@naver.com>
2024-09-25 23:52:36 +0900

    feat: GlobalExceptionHandler에 예외 추가(IllegalArgumentException, SecurityException, AccessDeniedException)

--- backend/src/main/java/com/ssafy/eggmoney/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/eggmoney/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ssafy.eggmoney.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +10,22 @@ import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // 토큰 없이 요청 시
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<String> handleUnauthorized(SecurityException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleForbidden(AccessDeniedException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
