a8d5753ae6938cfc11ffdcce47583c0ccaafb728
나경준 <na6037@naver.com>
2024-09-26 14:05:08 +0900

    feat:유저 업데이트 추가

--- backend/src/main/java/com/ssafy/eggmoney/user/entity/User.java
+++ backend/src/main/java/com/ssafy/eggmoney/user/entity/User.java
@@ -62,4 +62,10 @@ public class User extends BaseTime {
         this.stockRatio = ratio;
     }
 
+    public void updateUserInfo(String name, String bank, String realAccount, String simplePwd){
+        if(name != null) this.name = name;
+        if(bank!=null) this.bank = bank;
+        if(realAccount != null) this.realAccount = realAccount;
+        if(simplePwd != null) this.simplePwd = simplePwd;
+    }
 }
