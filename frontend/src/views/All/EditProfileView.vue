1f3abdc41375df39e3feef96871debbd2c02ea8f
나경준 <na6037@naver.com>
2024-09-26 09:50:34 +0900

    fix: 깃 충돌 수정 및 가족 페이지 용돈 설정

---
+++ frontend/src/views/All/EditProfileView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import EditProfileComponent from '@/components/login/EditProfileComponent.vue';
+
+
+
+
+</script>
+
+<template>
+  <div class="">
+    <EditProfileComponent/>
+    
+  </div>
+</template>
