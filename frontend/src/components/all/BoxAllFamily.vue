ef729ceee1ac99a528e3230db3ddbb3d004b191e
Soyoung <bayleaf07@naver.com>
2024-09-26 11:59:56 +0900

    feat: 전체 페이지 제작

---
+++ frontend/src/components/all/BoxAllFamily.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import IconRightArrow from "../icons/IconRightArrow.vue"
+import IconFamily from "../icons/IconFamily.vue"
+</script>
+
+<template>
+  <div class="bg-white m-4 rounded-lg shadow flex flex-col">
+    <div class="flex justify-between">
+      <div class="m-4">
+        <p class="text-lg flex items-center mt-3">
+          <IconFamily class="size-6 ml-4" />&nbsp; 가족관리
+        </p>
+      </div>
+      <div class="m-4 flex justify-center items-center">
+        <p></p>
+      </div>
+    </div>
+    <div class="flex justify-between cursor-pointer">
+      <div class="m-4">
+        <p>&nbsp;&nbsp;&nbsp;내 가족</p>
+      </div>
+      <div class="m-4 flex justify-center items-center">
+        <p><IconRightArrow class="size-6" /></p>
+      </div>
+    </div>
+
+    <!-- <div class="flex justify-between">
+      <div class="m-4">
+        <p>&nbsp;&nbsp;&nbsp;적금 상품 조회</p>
+      </div>
+      <div class="m-4 flex justify-center items-center">
+        <p><IconRightArrow class="size-6" /></p>
+      </div>
+    </div> -->
+
+    <!-- <div class="flex justify-between">
+      <div class="m-4">
+        <p>&nbsp;&nbsp;&nbsp;뉴스</p>
+      </div>
+      <div class="m-4 flex justify-center items-center">
+        <p><IconRightArrow class="size-6" /></p>
+      </div>
+    </div> -->
+
+    <div class="flex justify-between mb-3 cursor-pointer">
+      <div class="m-4">
+        <p>&nbsp;&nbsp;&nbsp;가족 연결</p>
+      </div>
+      <div class="m-4 flex justify-center items-center">
+        <p><IconRightArrow class="size-6" /></p>
+      </div>
+    </div>
+  </div>
+</template>
