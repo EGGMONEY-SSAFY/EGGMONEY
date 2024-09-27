8f24d532110998a8f40280d8a2f6aad691ddeac0
jiyoung <jiyoung990601@gmail.com>
2024-09-27 14:04:44 +0900

    feat: 대출신청페이지

---
+++ frontend/src/components/icons/IconQuestionMark.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-5 h-5 relative">
+    <div
+      class="absolute w-5 h-5 top-0 left-0 border-2 border-black rounded-full flex items-center justify-center"
+    >
+      <span class="text-black text-sm font-bold">?</span>
+    </div>
+  </div>
+</template>
