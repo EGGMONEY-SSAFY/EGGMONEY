334a7dfbeb12f70c242f4a116347636f47517859
jiyoung <jiyoung990601@gmail.com>
2024-09-27 09:27:27 +0900

    temp: 대출신청페이지 시작 전 풀

--- frontend/src/views/Fin/FinLoanCreateView.vue
+++ frontend/src/views/Fin/FinLoanCreateView.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconExplanation from "@/components/icons/IconExplanation.vue"
+</script>
 
 <template>
-  <div class="">
-    <div>대출신청하는 페이지</div>
+  <div class="m-4">
+    <div class="m-2 flex">
+      <IconExplanation></IconExplanation>
+    </div>
+    <div class="text-gray-600 font-bold text-sm">대출 신청서를 작성해보아요</div>
+
+    <div>
+      신청 사유가 무엇인가요?
+    </div>
+    <div></div>
   </div>
 </template>
