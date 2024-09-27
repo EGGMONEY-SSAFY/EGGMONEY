890a94204ada0425a94a09643edf544d21254bca
unknown <gje960217@naver.com>
2024-09-27 13:39:07 +0900

    feat: 뉴스 어느정도 완성

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -26,6 +26,7 @@ import FinDepositCreateDetailView from "@/views/Fin/FinDepositCreateDetailView.v
 import FinDepositCreateView from "@/views/Fin/FinDepositCreateView.vue"
 import FinSavingsCreateDetailView from "@/views/Fin/FinSavingsCreateDetailView.vue"
 import EditProfileView from "@/views/All/EditProfileView.vue"
+import StockOrderListView from "@/views/Stock/StockOrderListView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,11 @@ const router = createRouter({
       path: "/main",
       name: "MainView",
       component: MainView,
+    },
+    {
+      path: "/stock/order-list",
+      name: "StockOrderListView",
+      component: StockOrderListView,
     },
     {
       path: "/stock/news/:newsId",
@@ -102,7 +108,7 @@ const router = createRouter({
       ],
     },
     {
-      path: "/stock",
+      path: "/stock/home",
       name: "StockView",
       component: StockView,
     },
