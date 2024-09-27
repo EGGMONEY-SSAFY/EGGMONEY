ef5265aded77c4ce7b8383f2713dcdec055f1cf3
unknown <gje960217@naver.com>
2024-09-27 14:21:22 +0900

    style: 포메팅

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,10 @@
-import "@/assets/main.css"
-
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 import piniaPersistedstate from "pinia-plugin-persistedstate"
-
 import App from "./App.vue"
 import router from "./router"
 
 const app = createApp(App)
-
 const pinia = createPinia()
 pinia.use(piniaPersistedstate) // pinia-plugin-persistedstate 추가
 
