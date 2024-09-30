b0ee3c8c18c57eb2d15032296a05af39ad5288d3
나경준 <na6037@naver.com>
2024-09-30 12:13:46 +0900

    feat: 가족 연결 axios연결중

--- frontend/src/components/family/FamilyInviteQRCodeComponent.vue
+++ frontend/src/components/family/FamilyInviteQRCodeComponent.vue
@@ -10,21 +10,41 @@
 import { ref, onMounted } from "vue"
 // @ts-ignore
 import QRCode from "qrcode"
+import axios from "axios"
+import { useAuthStore } from "@/stores/auth"
 
+const authStore = useAuthStore()
 const familyId = ref("1")
 const qrCode = ref<string | null>(null)
-
+async function getfamilyId() {
+  // const token = authStore.accessToken;
+  const token = "HpAwXfMaEpHRVBLX6CvO2-LUlcUMjy1EAAAAAQorDR4AAAGSPfkK3pCBbdpZdq0Z"
+  try {
+    const response = await axios.get("http://localhost:8080/api/v1/profile/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+    console.log(response)
+  } catch (error) {
+    console.error("유저정보 로드 오류", error)
+  }
+}
 // Function to generate QR code
 async function generateQRCode() {
   try {
-    qrCode.value = await QRCode.toDataURL(`http://localhost:8080/invite?familyId=${familyId.value}`)
+    qrCode.value = await QRCode.toDataURL(
+      `http://localhost:8080/api/v1/family/${familyId.value}/join`
+    )
   } catch (error) {
     console.error("QR code generation error:", error)
   }
 }
 
-onMounted(() => {
-  generateQRCode()
+onMounted(async () => {
+  await getfamilyId()
+  await generateQRCode()
 })
 </script>
 
