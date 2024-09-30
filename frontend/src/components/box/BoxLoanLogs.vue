83d7cf3f9fc0bb904aff3f4b1cb61d0047b6cc88
WOOSEONG-1 <asdf1916@naver.com>
2024-09-30 12:59:47 +0900

    fix: Log 출력 에러 수정 및 Pagination hidden 속성 제거

--- frontend/src/components/box/BoxLoanLogs.vue
+++ frontend/src/components/box/BoxLoanLogs.vue
@@ -3,15 +3,14 @@ import { onMounted, ref, watch } from "vue"
 import type { LoanLog } from "@/stores/fin"
 
 const props = defineProps<{ history: LoanLog[] | null }>()
-let count = ref<number>(history.length)
-let index = 0
+
+let count = ref<number>(0)
 
 watch(
   () => props.history,
   (newHistory) => {
     if (newHistory && newHistory.length > 0) {
       count.value = newHistory.length
-      index = 0
     }
   },
   { immediate: true }
@@ -20,18 +19,16 @@ watch(
 onMounted(() => {
   if (props.history) {
     count.value = props.history.length
-    console.log("count : " + count.value)
-    index = 0
   }
 })
 </script>
 
 <template>
   <div class="bg-white m-4 rounded-lg py-3 mt-8" v-if="props.history">
-    <div v-for="log in props.history" :key="log.createdAt">
+    <div v-for="(log, idx) in props.history" :key="log.createdAt">
       <div class="m-4 flex justify-between px-5 py-1">
         <div class="text-start">
-          <h1 class="font-semibold text-lg">{{ count - index++ }} 회차</h1>
+          <h1 class="font-semibold text-lg">{{ count - idx }} 회차</h1>
           <h1>
             {{
               new Date(log.createdAt).toLocaleDateString("en-US", {
--- frontend/src/components/box/BoxSavingsLogs.vue
+++ frontend/src/components/box/BoxSavingsLogs.vue
@@ -3,22 +3,19 @@ import { onMounted, ref, watch } from "vue"
 import type { SavingsLogs } from "@/stores/fin"
 
 const props = defineProps<{ history: SavingsLogs[] | null }>()
-let count = ref<number>(history.length)
-let index = 0
+let count = ref<number>(0)
 
 watch(
   () => props.history,
   (newHistory) => {
     if (newHistory && newHistory.length > 0) {
       count.value = newHistory.length
-      index = 0
     }
   },
   { immediate: true }
 )
 
 onMounted(() => {
-  index = 0
   if (props.history) {
     count.value = props.history.length
   }
@@ -27,10 +24,10 @@ onMounted(() => {
 
 <template>
   <div class="bg-white m-4 rounded-lg py-3 mt-8" v-if="props.history">
-    <div v-for="log in props.history" :key="log.createdAt">
+    <div v-for="(log, idx) in props.history" :key="log.createdAt">
       <div class="m-4 flex justify-between px-5 py-1">
         <div class="text-start">
-          <h1 class="font-semibold text-lg">{{ count - index++ }} 회차</h1>
+          <h1 class="font-semibold text-lg">{{ count - idx }} 회차</h1>
           <h1>
             {{
               new Date(log.createdAt).toLocaleDateString("en-US", {
