ebe3633f213aeaaeec055af19421d2a199fb0c18
KimShinBeom <gotwo6141922@naver.com>
2024-09-30 21:49:46 +0900

    refactor: AccountService의 getAnalytics메소드에 보유 주식의 총 금액 추가

--- backend/src/main/java/com/ssafy/eggmoney/stock/repository/StockUserRepository.java
+++ backend/src/main/java/com/ssafy/eggmoney/stock/repository/StockUserRepository.java
@@ -3,8 +3,10 @@ package com.ssafy.eggmoney.stock.repository;
 import com.ssafy.eggmoney.stock.entity.StockUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StockUserRepository extends JpaRepository<StockUser, Long> {
     Optional<StockUser> findByStockIdAndUserId(Long stockId, Long userId);
+    List<StockUser> findByUserId(Long userId);
 }
