d99ac1bfb2343932ebfd21018a9bd83803c4c514
KimShinBeom <gotwo6141922@naver.com>
2024-10-02 00:59:11 +0900

    fix: 거래내역 조회 시 거래날짜 반환하도록 추가

--- backend/src/main/java/com/ssafy/eggmoney/stock/dto/response/StockLogResponse.java
+++ backend/src/main/java/com/ssafy/eggmoney/stock/dto/response/StockLogResponse.java
@@ -4,16 +4,20 @@ import com.ssafy.eggmoney.stock.entity.StockItem;
 import com.ssafy.eggmoney.stock.entity.TradeType;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class StockLogResponse {
     private StockItem stockItem;
+    private LocalDateTime tradeDate;
     private TradeType tradeType;
     private int price;
     private int amount;
     private int totalPrice;
 
-    public StockLogResponse(StockItem stockItem, TradeType tradeType, int price, int amount) {
+    public StockLogResponse(StockItem stockItem, LocalDateTime tradeDate, TradeType tradeType, int price, int amount) {
         this.stockItem = stockItem;
+        this.tradeDate = tradeDate;
         this.tradeType = tradeType;
         this.price = price;
         this.amount = amount;
