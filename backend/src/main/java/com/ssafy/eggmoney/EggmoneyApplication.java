57c80507ccfbb7b057adb75423d3bde94ef2a3cb
Soyoung <bayleaf07@naver.com>
2024-10-02 16:14:41 +0900

    배포테스트16

--- backend/src/main/java/com/ssafy/eggmoney/EggmoneyApplication.java
+++ backend/src/main/java/com/ssafy/eggmoney/EggmoneyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-// test112dddd22222
+// test112dddd2222222222
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
