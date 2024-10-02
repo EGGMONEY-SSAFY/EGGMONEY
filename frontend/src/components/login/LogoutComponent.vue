584f7d1a10faa5c6475ca3ee634d71e9ffd4d2c1
나경준 <na6037@naver.com>
2024-10-02 16:58:09 +0900

    feat: 로그아웃 추가

---
+++ frontend/src/components/login/LogoutComponent.vue
@@ -0,0 +1,17 @@
+<template >
+    <div>
+        <h1>로그아웃</h1>
+        <button @click="logout">Logout</button>
+    </div>
+</template>
+<script setup lang="ts">
+
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+const logout = ()=>{
+    authStore.logout();
+}
+
+
+</script>
