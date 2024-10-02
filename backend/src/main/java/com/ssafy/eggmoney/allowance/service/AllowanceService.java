c6eea7d25cc88be2a890a287d170f602a0c51ffe
나경준 <na6037@naver.com>
2024-10-02 12:15:09 +0900

    1002

--- backend/src/main/java/com/ssafy/eggmoney/allowance/service/AllowanceService.java
+++ backend/src/main/java/com/ssafy/eggmoney/allowance/service/AllowanceService.java
@@ -1,4 +1,49 @@
 package com.ssafy.eggmoney.allowance.service;
 
+
+import com.ssafy.eggmoney.allowance.dto.request.AllowanceGetRequestDto;
+import com.ssafy.eggmoney.allowance.dto.response.AllowanceCreateResponseDto;
+import com.ssafy.eggmoney.allowance.dto.response.AllowanceUpdateResponseDto;
+import com.ssafy.eggmoney.allowance.entity.Allowance;
+import com.ssafy.eggmoney.allowance.entity.AllowancePeriod;
+import com.ssafy.eggmoney.allowance.repository.AllowanceRepository;
+import com.ssafy.eggmoney.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class AllowanceService {
+
+    private final AllowanceRepository allowanceRepository;
+
+    public AllowanceCreateResponseDto createAllowance(User user){
+        if("자녀".equals(user.getRole())){
+            Allowance allowance = Allowance.createAllowance(user,0,null,0);
+            allowanceRepository.save(allowance);
+
+            return new AllowanceCreateResponseDto(allowance);
+        }
+        throw new IllegalArgumentException("user is not your child");
+    }
+
+    public AllowanceUpdateResponseDto updateAllowance(Long childId, AllowanceUpdateResponseDto updateDto){
+        Allowance allowance = allowanceRepository.findByChildId(childId)
+                .orElseThrow(()-> new IllegalArgumentException("User didnt have allowance"));
+        allowance.setPrice(updateDto.getPrice());
+        allowance.setAllowanceDay(updateDto.getAllowanceDay());
+        allowance.setAllowancePeriod(AllowancePeriod.valueOf(updateDto.getAllowancePeriod()));
+        allowanceRepository.save(allowance);
+
+        return new AllowanceUpdateResponseDto(allowance);
+    }
+
+    public List<AllowanceGetRequestDto> getAllowancesForChildren(User user){
+        return allowanceRepository.findAllByChildFamilyId(user.getFamily().getId()).stream()
+                .map(AllowanceGetRequestDto::new)
+                .collect(Collectors.toList());
+    }
 }
