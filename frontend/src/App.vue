59ccdf16046cc67b27379a2727f04534fbce745c
Soyoung <bayleaf07@naver.com>
2024-10-02 14:50:56 +0900

    배포테스트14

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ const finStore = useFinStore()
 </script>
 
 <template>
-  <!-- te555-->
+  <!-- tedddd555-->
   <div class="flex justify-center bg-gray-800">
     <div
       class="main-container bg-gray-200"
