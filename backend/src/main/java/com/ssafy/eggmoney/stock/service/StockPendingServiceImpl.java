80f3c0cbef2f6005857270f8d83d30a888f9b5d1
ShinBeomKim <shinbeomkim11@gmail.com>
2024-10-04 01:30:50 +0900

    feat: 지정매수&지정매도 예약 기능 추가

---
+++ backend/src/main/java/com/ssafy/eggmoney/stock/service/StockPendingServiceImpl.java
@@ -0,0 +1,50 @@
+package com.ssafy.eggmoney.stock.service;
+
+import com.ssafy.eggmoney.stock.dto.request.PendingRequest;
+import com.ssafy.eggmoney.stock.entity.Stock;
+import com.ssafy.eggmoney.stock.entity.StockPending;
+import com.ssafy.eggmoney.stock.entity.TradeType;
+import com.ssafy.eggmoney.stock.repository.StockPendingRepository;
+import com.ssafy.eggmoney.stock.repository.StockRepository;
+import com.ssafy.eggmoney.user.entity.User;
+import com.ssafy.eggmoney.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class StockPendingServiceImpl implements StockPendingService {
+    private final StockPendingRepository stockPendingRepository;
+    private final StockRepository stockRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public void saveStockPending(PendingRequest pendingReq, TradeType tradeType) {
+        User user = userRepository.findById(pendingReq.getUserId())
+                .orElseThrow(() -> new NoSuchElementException("해당 유저를 찾을 수 없습니다."));
+        Stock stock = stockRepository.findById(pendingReq.getStockId())
+                .orElseThrow(() -> new NoSuchElementException("해당 주식을 찾을 수 없습니다."));
+
+        stockPendingRepository.save(new StockPending(
+                user, stock, tradeType, pendingReq.getPendingPrice(), pendingReq.getPendingAmount())
+        );
+    }
+
+    @Override
+    public int findPendingBuyTotalPrice(Long userId) {
+        List<StockPending> stockPendings = stockPendingRepository.findByUserIdAndTradeType(userId, TradeType.BUY);
+
+        int totalPendigPrice = 0;
+        for(StockPending stockPending : stockPendings) {
+            totalPendigPrice += stockPending.getPendingPrice() * stockPending.getPendingAmount();
+        }
+
+        return totalPendigPrice;
+    }
+}
