07133d29a714b9f89d68a849f928434d4a8121ad
KimShinBeom <gotwo6141922@naver.com>
2024-10-03 17:14:05 +0900

    refactor: 엔티티 구조 변경에 따른 주식 매도, 매수, 해당 주식 보유 정보 수정

--- backend/src/main/java/com/ssafy/eggmoney/stock/repository/StockUserRepository.java
+++ backend/src/main/java/com/ssafy/eggmoney/stock/repository/StockUserRepository.java
@@ -1,23 +1,18 @@
 package com.ssafy.eggmoney.stock.repository;
 
-import com.ssafy.eggmoney.stock.entity.StockItem;
 import com.ssafy.eggmoney.stock.entity.StockUser;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StockUserRepository extends JpaRepository<StockUser, Long> {
-    Optional<StockUser> findByStockIdAndUserId(Long stockId, Long userId);
-    List<StockUser> findByUserId(Long userId);
-
     @EntityGraph(attributePaths = {"stock"})
     List<StockUser> findJoinStockByUserId(Long userId);
 
-    @Query("select su from StockUser su left join fetch su.stock s " +
-            "where su.user.id = :userId and s.stockItem = :item")
-    Optional<StockUser> findByUserIdAndStockItem(@Param("userId") Long userId, @Param("item") StockItem item);
+    @EntityGraph(attributePaths = {"stock"})
+    Optional<StockUser> findJoinStockByUserIdAndStockId(Long userId, Long stockId);
+
+    Optional<StockUser> findByUserIdAndStockId(Long userId, Long stockId);
 }
