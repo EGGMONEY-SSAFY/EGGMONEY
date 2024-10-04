f72645ef26b2193a0cc4269e2d5ea2d5d7e1f6fd
나경준 <na6037@naver.com>
2024-10-04 14:58:42 +0900

    feat: 프론트 토큰기반 막기

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router"
+import { RouterView, useRoute, useRouter } from "vue-router"
 import NavBar from "./components/navbar/NavBar.vue"
 import NavBarTop from "./components/navbar/NavBarTop.vue"
 import { useFinStore } from "./stores/fin"
 import { useAuthStore } from "@/stores/auth"
 import { onMounted } from "vue"
 const finStore = useFinStore()
-
+const route = useRoute() // 현재 경로를 가져옴
 const authStore = useAuthStore()
+const router = useRouter() 
 onMounted(() => {
-  authStore.loadTokens()
+  authStore.loadTokens(router)
 })
 </script>
 
 <template>
   <!-- test111111-->
-  <div class="flex justify-center bg-gray-800">
+  <!-- <div class="flex justify-center bg-gray-800">
     <div
       class="main-container bg-gray-200"
       :class="{ 'bg-yellow-50': finStore.isYellowPage, 'bg-gray-200': !finStore.isYellowPage }"
@@ -23,6 +24,18 @@ onMounted(() => {
       <NavBarTop />
       <RouterView class="mt-12 mb-20" />
       <NavBar />
+    </div>
+  </div> -->
+  <!-- test111111-->
+  <div class="flex justify-center bg-gray-800">
+    <div
+      class="main-container bg-gray-200"
+      :class="{ 'bg-yellow-50': finStore.isYellowPage, 'bg-gray-200': !finStore.isYellowPage }"
+    >
+      <!-- /main 또는 /login일 경우 상단바와 하단바 숨김 -->
+      <NavBarTop v-if="route.path !== '/main' && route.path !== '/login'" />
+      <RouterView class="mt-12 mb-20" />
+      <NavBar v-if="route.path !== '/main' && route.path !== '/login'" />
     </div>
   </div>
 </template>
