2b83f54076994f1eed0b3f3cd37b8197ad02832f
KimShinBeom <gotwo6141922@naver.com>
2024-10-04 18:07:29 +0900

    refactor:뉴스 gpt 프롬프트 변경

--- backend/src/main/java/com/ssafy/eggmoney/news/scheduler/NewsScheduler.java
+++ backend/src/main/java/com/ssafy/eggmoney/news/scheduler/NewsScheduler.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class NewsScheduler {
     private final NewsService newsService;
 
-//    @Scheduled(cron = "0 0 7 * * *", zone = "Asia/Seoul")
+//    @Scheduled(cron = "0 51 16 * * *", zone = "Asia/Seoul")
     public void saveSummarizeAndCrawlNews(){
         log.info("뉴스 스케쥴링 시작: " + LocalDateTime.now());
         List<News> newsList = new ArrayList<>();
@@ -28,7 +28,7 @@ public class NewsScheduler {
         for(NewsCrawlResponse headLineNews : headLineNewsList){
             News news = new News(headLineNews.getTitle(), headLineNews.getLink(), headLineNews.getPress());
             String newsContent = newsService.crawlNewsContent(headLineNews.getLink());
-            newsContent = "줄바꿈 X, 350토큰이상, 존댓말. 뉴스: " + newsContent;
+            newsContent = "문단 단위로 줄 바꿈, 300자이상, 존댓말. 뉴스: " + newsContent;
             SummarizedContentResponse summarizedContent = newsService.summarizeNews(newsContent);
 
             if(summarizedContent.getFinishReason() == null && summarizedContent.getRefusal() == null) {
