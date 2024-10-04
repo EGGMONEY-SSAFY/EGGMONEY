d434a5ae9cfd201ba040a413a87dabdd32997e02
Soyoung <bayleaf07@naver.com>
2024-10-04 13:35:17 +0900

    feat: 배포테스트21

--- backend/src/main/java/com/ssafy/eggmoney/EggmoneyApplication.java
+++ backend/src/main/java/com/ssafy/eggmoney/EggmoneyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-// test
+// test1111
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,7 +14,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- test-->
+  <!-- test111111-->
   <div class="flex justify-center bg-gray-800">
     <div
       class="main-container bg-gray-200"
