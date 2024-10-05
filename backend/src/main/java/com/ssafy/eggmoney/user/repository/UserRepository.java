77144ea4a874c540b58610099a59ef0e022cb793
ShinBeomKim <shinbeomkim11@gmail.com>
2024-10-05 18:11:36 +0900

    feat: 자녀의 투자비율 설정 기능 추가

--- backend/src/main/java/com/ssafy/eggmoney/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/eggmoney/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.eggmoney.user.repository;
 
 import com.ssafy.eggmoney.family.entity.Family;
 import com.ssafy.eggmoney.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -16,6 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     List<User> findAllByFamilyId(long familyId);
 
-    @Query("select u from User u where u.id = :childId and u.family.id = :familyId")
-    Optional<Family> findChild(@Param("familyId") Long familyId, @Param("childId") Long childId);
+    @EntityGraph(attributePaths = {"family"})
+    Optional<User> findJoinFamilyById(long id);
 }
