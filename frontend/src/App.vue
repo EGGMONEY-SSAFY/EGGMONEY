2a3aba9038b6b11177ea3755169123285299e4b3
Soyoung <bayleaf07@naver.com>
2024-10-06 14:18:47 +0900

    feat : 배포테스트 41

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,7 +24,7 @@ onMounted( async () => {
 </script>
 
 <template>
-  <!-- test22-->
+  <!-- test2244444 -->
   <!-- <div class="flex justify-center bg-gray-800">
     <div
       class="main-container bg-gray-200"
