a8e5c4de1632857d27e4746067ff725ce0d85950
jiyoung <jiyoung990601@gmail.com>
2024-10-07 17:06:38 +0900

    fix: 대출 스케줄러 오류 수정

--- backend/src/main/java/com/ssafy/eggmoney/loan/entity/LoanStatus.java
+++ backend/src/main/java/com/ssafy/eggmoney/loan/entity/LoanStatus.java
@@ -4,5 +4,6 @@ public enum LoanStatus {
     PROGRESS, // 진행중
     APPROVAL, // 승인
     REFUSAL, // 거절
-    EXPIRED; // 상환완료
+    EXPIRED,
+    ; // 상환완료
 }
--- backend/src/main/java/com/ssafy/eggmoney/loan/repository/LoanRepository.java
+++ backend/src/main/java/com/ssafy/eggmoney/loan/repository/LoanRepository.java
@@ -12,11 +12,11 @@ import java.util.Optional;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-    @Query(value = "SELECT * FROM loans l WHERE l.user_id IN (:ids) ORDER BY l.created_at DESC", nativeQuery = true)
+    @Query(value = "SELECT l FROM Loan l WHERE l.user.id IN (:ids) ORDER BY l.createdAt DESC")
     List<Loan> findAllByUserIdOrderByCreatedAtDesc(@Param("ids") List<Long> userIds);
 
     Optional<Loan> findByIdAndLoanStatus(Long id, LoanStatus loanStatus);
 
-//    @Query(value = "SELECT")
+    @Query(value = "SELECT l.id FROM Loan l WHERE l.loanStatus = :loanStatus AND l.expirationDate BETWEEN :start AND :end")
     List<Long> findIdByLoanStatusAndExpirationDateBetween(LoanStatus loanStatus, LocalDateTime start, LocalDateTime end);
 }
