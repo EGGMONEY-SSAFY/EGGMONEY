d5d32648cc4d54e12ce92dcb3a3c7099d124d08b
나경준 <na6037@naver.com>
2024-10-08 08:35:34 +0900

    feat:backend S3설정 수정

--- backend/src/main/java/com/ssafy/eggmoney/common/service/S3Service.java
+++ backend/src/main/java/com/ssafy/eggmoney/common/service/S3Service.java
@@ -1,14 +1,19 @@
 package com.ssafy.eggmoney.common.service;
 
+import com.ssafy.eggmoney.common.config.S3Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
 
 
@@ -17,34 +22,53 @@ public class S3Service {
     private final S3Client s3Client;
     private final String bucketName;
 
-    public S3Service(@Value("${cloud.aws.credentials.accessKey}") String accessKey,
-                     @Value("${cloud.aws.credentials.secretKey}") String secretKey,
-                     @Value("${cloud.aws.region.static}") String region,
-                     @Value("${cloud.aws.region.static}") String bucketName){
+    public S3Service(S3Client s3Client, @Value("${cloud.aws.s3.bucket}") String bucketName) {
+        this.s3Client = s3Client;
         this.bucketName = bucketName;
-
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-        this.s3Client = S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .build();
     }
+    public String uploadFile(MultipartFile file) throws IOException{
+        String key = Paths.get("family-profiles", file.getOriginalFilename()).toString();
 
-    public void uploadFile(String fileName, String filePath){
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key(fileName)
+                .key(key)
                 .build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
-        s3Client.putObject(putObjectRequest, Paths.get(filePath));
+        return key;
     }
-
-    public void downloadFile(String fileName, String destinationPath){
+    public InputStream downloadFiles(String key){
         GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                 .bucket(bucketName)
-                .key(fileName)
+                .key(key)
                 .build();
-
-        s3Client.getObject(getObjectRequest, Paths.get(destinationPath));
+        return s3Client.getObject(getObjectRequest);
     }
+
 }
+//        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+//                S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+//                .build();
+//            @Value("${cloud.aws.credentials.accessKey}") String accessKey,
+//            @Value("${cloud.aws.credentials.secretKey}") String secretKey,
+//            @Value("${cloud.aws.region.static}") String region,
+//            @Value("${cloud.aws.region.static}") String bucketName
+//public void uploadFile(String fileName, String filePath){
+//    PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+//            .bucket(bucketName)
+//            .key(fileName)
+//            .build();
+//
+//    s3Client.putObject(putObjectRequest, Paths.get(filePath));
+//}
+//
+//public void downloadFile(String fileName, String destinationPath){
+//    GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+//            .bucket(bucketName)
+//            .key(fileName)
+//            .build();
+//
+//    s3Client.getObject(getObjectRequest, Paths.get(destinationPath));
+//}
