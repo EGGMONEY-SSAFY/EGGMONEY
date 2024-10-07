9f70e5f864b51bb8b10f727502b672d6c5bfaea5
unknown <gje960217@naver.com>
2024-10-07 17:55:44 +0900

    style: 뉴스 줄바꿈 적용

--- frontend/src/components/box/BoxNewsDetail.vue
+++ frontend/src/components/box/BoxNewsDetail.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex flex-col m-4 bg-white rounded-lg shadow">
+  <div class="flex flex-col m-4 bg-white rounded-lg shadow" v-if="props.article?.content">
     <div class="m-4 text-2xl text-center">
       <span class="font-bold">{{ props.article?.title }}</span>
     </div>
@@ -14,8 +14,8 @@ const props = defineProps({
       <span>{{ props.article?.publishDate }}</span>
       <a :href="props.article?.link" target="_blank" rel="noopener noreferrer">원문보기</a>
     </div>
-    <div class="m-4">
-      <span>{{ props.article?.content }}</span>
+    <div class="m-4" v-for="paragraph in props.article?.content.split('\n\n')" :key="paragraph">
+      {{ paragraph }}
     </div>
   </div>
 </template>
