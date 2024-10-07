e119a6212c0f9547731f6ce19fbf61b67c6a6f6a
jiyoung <jiyoung990601@gmail.com>
2024-10-07 16:32:59 +0900

    fix: 적금 스케줄러 오류 수정

--- backend/src/main/java/com/ssafy/eggmoney/savings/repository/SavingsLogRepository.java
+++ backend/src/main/java/com/ssafy/eggmoney/savings/repository/SavingsLogRepository.java
@@ -2,11 +2,14 @@ package com.ssafy.eggmoney.savings.repository;
 
 import com.ssafy.eggmoney.savings.entity.SavingsLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SavingsLogRepository extends JpaRepository<SavingsLog, Long> {
     List<SavingsLog> findAllBySavingsIdOrderByCreatedAtDesc(Long savingsId);
+
+    @Query(value = "SELECT DISTINCT log.savings.id FROM SavingsLog log WHERE log.createdAt BETWEEN :start AND :end")
     List<Long> findSavingsIdByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
