f5152c575555d7880434474440a05225b8a8eb32
Soyoung <bayleaf07@naver.com>
2024-10-08 20:57:07 +0900

    feat: quiz

--- frontend/src/views/All/ReviewPage.vue
+++ frontend/src/views/All/ReviewPage.vue
@@ -1,27 +1,4 @@
 <template>
-    <div>
-      <h1>오답 리뷰</h1>
-      <div v-for="(question, index) in incorrectAnswers" :key="index">
-        <h2>문제 {{ index + 1 }}</h2>
-        <p>{{ question.content }}</p>
-        <p>정답: {{ question.answer }}</p>
-      </div>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted } from "vue";
-  import { useRoute } from "vue-router";
-  
-  const route = useRoute();
-  const incorrectAnswers = ref([]);
-  
-  onMounted(() => {
-    // Get the incorrect answers from the route query
-    const answers = route.query.incorrectAnswers;
-    if (answers) {
-      incorrectAnswers.value = JSON.parse(answers);
-    }
-  });
-  </script>
-  
+    <h1>오답 리뷰</h1>
+</template>
+
