8a6a9c2519f2ae1a50531c48366d626c53960778
KimShinBeom <gotwo6141922@naver.com>
2024-10-08 10:09:28 +0900

    feat : NotificationRequest에 생성자 추가

--- backend/src/main/java/com/ssafy/eggmoney/notification/dto/request/NotificationRequest.java
+++ backend/src/main/java/com/ssafy/eggmoney/notification/dto/request/NotificationRequest.java
@@ -1,11 +1,20 @@
 package com.ssafy.eggmoney.notification.dto.request;
 
 import com.ssafy.eggmoney.notification.entity.NotificationType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationRequest {
     private NotificationType notificationType;
     private String message;
     private Long receiveUser;
+
+    public NotificationRequest(NotificationType notificationType, String message) {
+        this.notificationType = notificationType;
+        this.message = message;
+    }
 }
