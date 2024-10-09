298aa350467a72ce10ca3c39c057b3d4ff1d1cf6
황우성 <asdf1916@naver.com>
2024-10-10 01:40:07 +0900

    feat: K-means 알고리즘 기반 사용자 군집별 상품 추천 서비스

--- fastapi/backend/utils.py
+++ fastapi/backend/utils.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+import time
 
 # SparkSession 생성
 def create_spark_session():
     spark = SparkSession.builder \
-        .appName("FastAPI HDFS Reader") \
+        .appName("FastAPI") \
         .getOrCreate()
     return spark
 
