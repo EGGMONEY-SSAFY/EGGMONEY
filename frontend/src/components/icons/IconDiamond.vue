62e49a54bf2adeb4a7e87236992adbbe5d945d1a
KimShinBeom <gotwo6141922@naver.com>
2024-10-10 11:19:31 +0900

    style : 대출 신청 아이콘 변경

--- frontend/src/components/icons/IconDiamond.vue
+++ frontend/src/components/icons/IconDiamond.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="flex items-center justify-center">
-    <!-- Outer White Diamond (small size) -->
+  <div class="w-5 h-5 relative">
     <div
-      class="w-3.5 h-3.5 transform rotate-45 bg-white border-2 border-black flex items-center justify-center"
+      class="absolute w-5 h-5 top-0 left-0 border-2 border-gray-700 rounded-full flex items-center justify-center"
     >
-      <!-- Inner Black Diamond -->
-      <div class="w-1.5 h-1.5 transform bg-black border-2 border-black"></div>
+      <span class="text-gray-700 text-sm font-bold">!</span>
     </div>
   </div>
 </template>
