965f03eef792ec25a3c9a52b6c0b265b49075ff5
황우성 <asdf1916@naver.com>
2024-10-10 15:18:24 +0900

    fix:front

--- frontend/src/views/All/MyRecommendView.vue
+++ frontend/src/views/All/MyRecommendView.vue
@@ -57,7 +57,6 @@ const getDepsoitProduct = function (id: number): Promise<void> {
   })
     .then((res) => {
       recommendDeposit.value = res.data
-      recommendDeposit.value.id = res.data.id
       console.log(res.data)
     })
     .catch((err) => {
