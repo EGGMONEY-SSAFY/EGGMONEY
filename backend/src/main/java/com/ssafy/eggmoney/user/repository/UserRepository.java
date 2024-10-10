472bcef2202730526f2e2a667abad04f697f6f54
ShinBeomKim <shinbeomkim11@gmail.com>
2024-10-10 14:28:50 +0900

    fix: 투자 가능 금액과 보유 주식 금액 조회 자녀 아이디를 받도록 변경 및 같은 가족인 지 검증 로직 추가

--- backend/src/main/java/com/ssafy/eggmoney/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/eggmoney/user/repository/UserRepository.java
@@ -7,10 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -21,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @EntityGraph(attributePaths = {"family"})
     Optional<User> findJoinFamilyById(long id);
+
+    Boolean existsByIdAndFamilyId(Long userId, Long familyId);
 }
