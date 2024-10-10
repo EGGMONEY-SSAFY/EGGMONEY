8bc828ee4eba077e4c800c0d3e1f5421e2f561c9
Soyoung <bayleaf07@naver.com>
2024-10-11 05:20:09 +0900

    feat: 배포테스트

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@ import { useAuthStore } from "@/stores/auth"
 import { onMounted, ref } from "vue"
 import { useUserStore } from "./stores/user"
 const finStore = useFinStore()
-const route = useRoute() // 현재 경로를 가져옴1232
+const route = useRoute() // 현재 경로를 가져옴1232222222222
 const authStore = useAuthStore()
 const router = useRouter()
 const userStore = useUserStore()
